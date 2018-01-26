7108db4d8431c651b781022746df5fdaea93104b
MonikaJ <monika.jelinkova@evalue.cz>
2018-01-26 19:05:39 +0100

    remove dummy expanse data

--- src/app.js
+++ src/app.js
@@ -13,9 +13,8 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+
+//store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 //store.dispatch(setTextFilter('bill'));
 // store.dispatch(setTextFilter('water'));
 
@@ -23,9 +22,9 @@ store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 //     store.dispatch(setTextFilter('bill'));
 // }, 3000)  po 3 vteřináchh změní text: bill
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 //console.log(store.getState());
 
